aa3a5609ddfb24fe64db1675cd25288a1c1f8842
ilv cheng <3556203242@qq.com>
2024-07-11 21:41:29 +0800

    跨域问题

---
+++ new_kob/src/main/java/com/kob/backend/service/impl/user/account/InfoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.kob.backend.service.impl.user.account;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfoServiceImpl {
+}
--- web/src/App.vue
+++ web/src/App.vue
@@ -7,9 +7,26 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import $ from "jquery"
 export default{
   components:{
     NavBar
+  },
+  setup() {
+    $.ajax({
+      url:"http://localhost:3000/user/account/token/",
+      type:"post",
+      data: {
+        username:'yxc',
+        password:"pyxc",
+      },
+      success(res){
+        console.log(res)
+      },
+      error(res){
+        console.log(res)
+      }
+    })
   }
 }
 </script>
